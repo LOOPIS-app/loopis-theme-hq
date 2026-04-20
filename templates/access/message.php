a90201515844b4bec0a71b8d82a4ad0afab5b19f
joxyzan <johan.hagvil@gmail.com>
2026-04-20 19:35:53 +0200

    access fix

--- templates/access/message.php
+++ templates/access/message.php
@@ -17,8 +17,19 @@ $bagis_link = home_url("/faq/varfor-bagis/");
 
 if (is_user_logged_in()) { 
 
+    $current_user = wp_get_current_user();
+    $user_roles = (array) $current_user->roles;
+
+    // Member
+    if (in_array('member', $user_roles, true)) {
+        $message = '<div class="wpum-message information">
+                    <p>Gå till ditt område:</p>
+                    <p><span class="link"><a href="'.esc_url( home_url( '/12845/' ) ).'">📍 Bagarmossen</a></span>&nbsp;<span class="link"><a href="'.esc_url( home_url( '/12833/' ) ).'">📍 Skarpnäck</a></span></p>
+                    </div>';
+    }
+
     // Member pending
-    if (current_user_can('member_pending')) {
+    if (in_array('member_pending', $user_roles, true)) {
         $message = '<div class="wpum-message information">
                     <p>🙏 Tack för din ansökan om medlemskap!</p>
                     <p class="small">När vi har registrerat din medlemsavgift får du ett mail.</p>
@@ -26,7 +37,7 @@ if (is_user_logged_in()) {
     }
 
     // Member earlier
-    elseif (current_user_can('member_earlier')) {
+    elseif (in_array('member_earlier', $user_roles, true)) {
         $message = '<div class="wpum-message warning">
                     <p>Du behöver förnya ditt medlemskap för att fortsätta använda LOOPIS. ✨</p>
                     <p><span class="big-link"><a href="'.esc_url( $renew_link ).'">🌈 Förnya medlemskap</a></span></p>
@@ -34,11 +45,19 @@ if (is_user_logged_in()) {
     }
 
     // Member outside
-    elseif (current_user_can('member_outside')) {
+    elseif (in_array('member_outside', $user_roles, true)) {
         $message = '<div class="wpum-message information">
                     <p>🙏 Tack för att du stöttar LOOPIS med ditt medlemskap!</p>
                     <p>Vi hoppas att du i framtiden kan använda föreningens tjänster där du bor.</p>
                     <p><span class="link"><a href="'.esc_url( $bagis_link ).'">📌 Varför måste jag bo i Bagarmossen?</a></span></p>
+                    </div>';
+    }
+
+    // Admin
+    elseif (in_array('administrator', $user_roles, true)) {
+        $message = '<div class="wpum-message information">
+                    <p>🐙 Du är inloggad som superadmin.</p>
+                    <p><span class="link"><a href="'.esc_url( home_url( '/12845/' ) ).'">📍 Bagarmossen</a></span>&nbsp;<span class="link"><a href="'.esc_url( home_url( '/12833/' ) ).'">📍 Skarpnäck</a></span>&nbsp;<span class="link"><a href="'.esc_url( home_url( '/wp-admin/' ) ).'">🔧 WP-admin</a></span></p>
                     </div>';
     }
 
