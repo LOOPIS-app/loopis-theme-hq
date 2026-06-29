7a8b5785a8ea6087d67b5482e7b6e1e2924e6b8b
joxyzan <johan.hagvil@gmail.com>
2026-06-29 18:11:07 +0200

    signup faq

--- templates/wp-signup/loopis-signup-title.php
+++ templates/wp-signup/loopis-signup-title.php
@@ -16,6 +16,10 @@ if (!defined('ABSPATH')) {
 3⃣ Logga in<br>
 4⃣ Bli medlem<br>
 
+<p>För att använda LOOPIS behöver du vara medlem i föreningen LOOPIS. Medlemmarna bestämmer medlemsavgiften på årsmötet.</p>
+<p><b>Skapa ett konto för att logga in och betala 50 kronor!</b></p>
+<p><span class="big-link"><a href="<?php echo esc_url(home_url('/faq/varfor-medlemskap/')); ?>">📌 Varför måste jag vara medlem?</a></span></p>
+<p><span class="big-link"><a href="<?php echo esc_url(home_url('/faq/loopis-stadgar/')); ?>">📜 Stadgar</a></span>&nbsp; <span class="big-link"><a href="<?php echo esc_url(home_url('/privacy/')); ?>">🗄 Integritet</a></span></p>
 <div class="loopis-message warning">
-<p>⚠ OBS! Du kan bara använda LOOPIS nära Bagarmossen! <span class="link"><a href="<?php echo esc_url(home_url('/faq/varfor-bagis/')); ?>">📌 Varför måste jag bo i Bagis?</a></span></p>
+<p>⚠ OBS! Du kan just nu bara använda LOOPIS nära Bagarmossen! <span class="big-link"><a href="<?php echo esc_url(home_url('/faq/varfor-bagis/')); ?>">📌 Varför måste jag bo i Bagis?</a></span></p>
 </div>
