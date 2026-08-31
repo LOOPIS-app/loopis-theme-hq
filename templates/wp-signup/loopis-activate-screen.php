b9ebc40b3e8fe1d7c6086a47d3916fae8681b20f
Johan Hagvil <119631988+joxyzan@users.noreply.github.com>
2026-08-31 17:22:38 +0200

    p24 fixes (#8)
    * access fixes
    
    * access fix
    
    * free signup fix

--- templates/wp-signup/loopis-activate-screen.php
+++ templates/wp-signup/loopis-activate-screen.php
@@ -9,11 +9,18 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 
+// Check cookie for special invite code
+if (empty($_COOKIE['special_invite_payload'])) {
 $login_url = isset($login_url) ? (string) $login_url : wp_login_url(home_url('/shop/?option=membership-stripe'));
+} else {
+    $login_url = isset($login_url) ? (string) $login_url : wp_login_url(home_url('/special-signup/'));
+}
+
 $user_login = isset($user_login) ? (string) $user_login : '';
 $user_email = isset($user_email) ? (string) $user_email : '';
 $password = isset($password) ? (string) $password : '';
 ?>
+
 <p style="opacity: 0.5;">✅ Skapa konto<br>
 ✅ Bekräfta e-post</p>
 <h1>3⃣ Logga in</h1>
@@ -21,7 +28,7 @@ $password = isset($password) ? (string) $password : '';
 <p style="opacity: 0.5;">4⃣ Bli medlem</p>
 <div class="wp-activate-message">
 <p>Ditt konto är aktiverat! 🥳<br>
-Logga in för att bli medlem i föreningen och börja loopa.</p>
+Logga in för att avsluta din registrering.</p>
 <p>E-post: <b><?php echo esc_html($user_email); ?></b></p>
 <p>Användarnamn: <b><?php echo esc_html($user_login); ?></b></p>
 <p>Lösenord: <b><?php echo esc_html($password); ?></b></p>
