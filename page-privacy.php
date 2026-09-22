af46227ac3476f71bea1939a0a3aafd498892ad1
joxyzan <johan.hagvil@gmail.com>
2026-09-22 17:51:19 +0200

    move file

--- page-privacy.php
+++ page-privacy.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Privacy page
+ * Page for LOOPIS privacy policy
  */
 ?>
 
 <?php get_header(); ?>
 
 <div class="page-padding center">
-<?php include LOOPIS_THEME_HQ_DIR . '/templates/privacy.php'; ?>
+<?php include LOOPIS_THEME_HQ_DIR . '/templates/faq/loopis-privacy.php'; ?>
 </div><!--page-padding center-->
 
 <?php get_footer();
